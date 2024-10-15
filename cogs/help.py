4eaaf1641b5b22f2768e5ca3ca782e7f1264dae6
HollowSeS <58048813+DoctorSeS@users.noreply.github.com>
2024-10-15 18:38:40 +0300

    Add files via upload

--- cogs/help.py
+++ cogs/help.py
@@ -133,7 +133,7 @@ class Pages(discord.ui.View):
     return ses2
   
   def setauthor(self):
-    self.sescheck()
+    Pages.sescheck(self)
     #set author#
     embed1.set_author(name=f"Consider supporting this project on patreon", icon_url=f"{ses2.avatar}", url="https://www.patreon.com/doctorses")
     embed2.set_author(name=f"Consider supporting this project on patreon", icon_url=f"{ses2.avatar}", url="https://www.patreon.com/doctorses")
@@ -352,7 +352,7 @@ class Pages_slash(discord.ui.View):
     return ses2
   
   def setauthor(self):
-    self.sescheck()
+    Pages_slash.sescheck(self)
     #set author#
     embed1.set_author(name=f"Consider supporting this project on patreon", icon_url=f"{ses2.avatar}", url="https://www.patreon.com/doctorses")
     embed2.set_author(name=f"Consider supporting this project on patreon", icon_url=f"{ses2.avatar}", url="https://www.patreon.com/doctorses")
